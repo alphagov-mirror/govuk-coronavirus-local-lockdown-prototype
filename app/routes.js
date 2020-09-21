13a8bd7684c001fc1c7ec2abfe79245135b9c4c4
Simon Whatley <simonwhatley@gmail.com>
2020-09-21 12:27:34 +0100

    Initial setup

---
+++ app/routes.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const express = require('express')
+const router = express.Router()
+
+// --------------------------------------------------
+// Add routes above this line
+// --------------------------------------------------
+module.exports = router
