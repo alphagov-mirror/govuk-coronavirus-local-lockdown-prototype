de189e2dbb551ceaa6083c8ad53d585d47878299
Simon Whatley <simonwhatley@gmail.com>
2020-09-21 13:01:27 +0100

    Version 1

--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,25 @@
 const express = require('express')
 const router = express.Router()
 
+router.get('/', (req, res) => {
+  delete req.session.data;
+
+  res.render('start', {
+    actions: {
+      save: `${req.baseUrl}/results`
+    }
+  })
+})
+
+router.post('/results', (req, res) => {
+
+  res.render('results', {
+    actions: {
+      back: `${req.baseUrl}/`
+    }
+  })
+})
+
 // --------------------------------------------------
 // Add routes above this line
 // --------------------------------------------------
