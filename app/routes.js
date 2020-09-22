094f00eaff1edd4c0cebd8f6882be51e25446c0a
Simon Whatley <simonwhatley@gmail.com>
2020-09-22 14:15:54 +0100

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -3,7 +3,7 @@
 const express = require('express')
 const router = express.Router()
 
-const Restrictions = require('./models/restrictions')
+// const Restrictions = require('./models/restrictions')
 
 // const https = require('https')
 // const fetch = require('node-fetch')
