220c93f7ca11503078196ae62d0b133cf619ca25
Simon Whatley <simonwhatley@gmail.com>
2020-09-22 11:56:40 +0100

    Add validation

--- app/routes.js
+++ app/routes.js
@@ -3,27 +3,57 @@
 const express = require('express')
 const router = express.Router()
 
-const https = require('https')
-const fetch = require('node-fetch')
+// const https = require('https')
+// const fetch = require('node-fetch')
+//
+// const MapIt = require('./models/mapit')
 
-const MapIt = require('./models/mapit')
-
-function checkHasAnswers (req, res, next) {
-  if (req.session.data.postcode === undefined) {
-    res.redirect(`${req.baseUrl}/`)
-  } else {
-    next()
-  }
-}
+// function checkHasAnswers (req, res, next) {
+//   if (req.session.data.postcode === undefined) {
+//     res.redirect(`${req.baseUrl}/`)
+//   } else {
+//     next()
+//   }
+// }
 
 router.get('/', (req, res) => {
   delete req.session.data;
 
   res.render('start', {
     actions: {
-      save: `${req.baseUrl}/results`
+      save: `${req.baseUrl}/`
     }
   })
+})
+
+router.post('/', (req, res) => {
+  const errors = []
+  const regex = /^([A-PR-UWYZ0-9][A-HK-Y0-9][AEHMNPRTVXY0-9]?[ABEHMNPRVWXY0-9]? {1,2}[0-9][ABD-HJLN-UW-Z]{2}|GIR 0AA)$/
+
+  if (!req.session.data.postcode.length) {
+    const error = {}
+    error.fieldName = 'postcode'
+    error.href = '#postcode'
+    error.text = 'Enter a postcode'
+    errors.push(error)
+  } else if (!regex.test(req.session.data.postcode)) {
+    const error = {}
+    error.fieldName = 'postcode'
+    error.href = '#postcode'
+    error.text = 'Enter a valid postcode'
+    errors.push(error)
+  }
+
+  if (errors.length) {
+    res.render('start', {
+      errors: errors,
+      actions: {
+        save: `${req.baseUrl}/`
+      }
+    })
+  } else {
+    res.redirect(`${req.baseUrl}/results`)
+  }
 })
 
 router.post('/results', (req, res) => {
@@ -53,6 +83,13 @@ router.post('/results', (req, res) => {
     // }
 
     // console.log('data', getData(url));
+
+    // res.render('results', {
+    //   actions: {
+    //     back: `${req.baseUrl}/`
+    //   },
+    //   area: MapIt.getArea(req.session.data.postcode, 'council_county')
+    // })
 
     res.render('results', {
       actions: {
