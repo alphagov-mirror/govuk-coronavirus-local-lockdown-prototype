6a2fdbaaf529191504402ec1d6e44842bee1a8fa
Simon Whatley <simonwhatley@gmail.com>
2020-10-06 14:25:57 +0100

    Add MongoDB

--- app/routes.js
+++ app/routes.js
@@ -3,9 +3,9 @@
 const express = require('express')
 const router = express.Router()
 
-const fetch = require('node-fetch')
+// const fetch = require('node-fetch')
 
-// const MapIt = require('./models/mapit')
+const Postcode = require('./models/postcode')
 const Restrictions = require('./models/restrictions')
 
 function checkHasPostcode (req, res, next) {
@@ -18,9 +18,6 @@ function checkHasPostcode (req, res, next) {
 
 router.get('/', (req, res) => {
   delete req.session.data
-
-  // console.log(Restrictions.find());
-  // console.log(MapIt.find('AL5 1ND'));
 
   res.render('start', {
     actions: {
@@ -60,6 +57,34 @@ router.post('/', (req, res) => {
 })
 
 router.get('/results', checkHasPostcode, (req, res) => {
+// .find({ postcode: req.session.data.postcode })
+// .aggregate([ { $search: { 'text': { 'query': req.session.data.postcode, 'path': 'postcode' } } }, { $limit: 1 }, { $project: { 'postcode': 1 } } ])
+  Postcode
+    .find({ postcode: req.session.data.postcode })
+    .then(data => {
+      console.log(data)
+      console.log(data[0].district_code)
+
+      const code = data[0].district_code
+
+      const restriction = Restrictions.findById(code)
+
+      console.log(restriction)
+
+      res.render('results', {
+        actions: {
+          back: `${req.baseUrl}/`
+        },
+        location: data[0],
+        restriction: restriction
+      })
+
+    })
+    .catch(err => {
+      console.log('ERROR 💥:', err)
+    })
+
+
 
   // const data = MapIt.find(req.session.data.postcode)
   //
@@ -70,63 +95,63 @@ router.get('/results', checkHasPostcode, (req, res) => {
   //   content: data
   // })
 
-  let url = `https://mapit.mysociety.org/postcode/${req.session.data.postcode}`
-
-  if (process.env.MAPIT_API_KEY !== undefined && process.env.MAPIT_API_KEY.length) {
-    url = `https://mapit.mysociety.org/postcode/${req.session.data.postcode}?api_key=${process.env.MAPIT_API_KEY}`
-  }
-
-  console.log(url)
-
-  fetch(url)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data)
-      const restrictions = Restrictions.find()
-      const locations = []
-      const lockdowns = []
-
-      if (data.areas !== undefined) {
-        // loop over the area data pulling out the Government Statistical Service (GSS) code for the area
-        for (const property in data.areas) {
-          if (data.areas[property].codes.gss !== undefined) {
-            locations.push(data.areas[property].codes.gss)
-          }
-        }
-
-        console.log(locations)
-
-        // loop over the GSS location codes to find the restriction for an area
-        locations.forEach(location => {
-          if (restrictions[location] !== undefined) {
-            lockdowns.push(restrictions[location])
-            console.log(restrictions[location])
-          } else {
-            // no restrictions
-            console.log(location + ' not found')
-          }
-        })
-
-        console.log(lockdowns)
-      }
-
-
-      res.render('results', {
-        actions: {
-          back: `${req.baseUrl}/`
-        },
-        content: lockdowns[0]
-      })
-    })
-    .catch(error => {
-      console.log(error)
-      // render an error page, or the results page with an error state
-      res.render('results', {
-        actions: {
-          back: `${req.baseUrl}/`
-        }
-      })
-    })
+  // let url = `https://mapit.mysociety.org/postcode/${req.session.data.postcode}`
+  //
+  // if (process.env.MAPIT_API_KEY !== undefined && process.env.MAPIT_API_KEY.length) {
+  //   url = `https://mapit.mysociety.org/postcode/${req.session.data.postcode}?api_key=${process.env.MAPIT_API_KEY}`
+  // }
+  //
+  // console.log(url)
+  //
+  // fetch(url)
+  //   .then(res => res.json())
+  //   .then(data => {
+  //     console.log(data)
+  //     const restrictions = Restrictions.find()
+  //     const locations = []
+  //     const lockdowns = []
+  //
+  //     if (data.areas !== undefined) {
+  //       // loop over the area data pulling out the Government Statistical Service (GSS) code for the area
+  //       for (const property in data.areas) {
+  //         if (data.areas[property].codes.gss !== undefined) {
+  //           locations.push(data.areas[property].codes.gss)
+  //         }
+  //       }
+  //
+  //       console.log(locations)
+  //
+  //       // loop over the GSS location codes to find the restriction for an area
+  //       locations.forEach(location => {
+  //         if (restrictions[location] !== undefined) {
+  //           lockdowns.push(restrictions[location])
+  //           console.log(restrictions[location])
+  //         } else {
+  //           // no restrictions
+  //           console.log(location + ' not found')
+  //         }
+  //       })
+  //
+  //       console.log(lockdowns)
+  //     }
+  //
+  //
+  //     res.render('results', {
+  //       actions: {
+  //         back: `${req.baseUrl}/`
+  //       },
+  //       content: lockdowns[0]
+  //     })
+  //   })
+  //   .catch(error => {
+  //     console.log(error)
+  //     // render an error page, or the results page with an error state
+  //     res.render('results', {
+  //       actions: {
+  //         back: `${req.baseUrl}/`
+  //       }
+  //     })
+  //   })
 })
 
 // --------------------------------------------------
