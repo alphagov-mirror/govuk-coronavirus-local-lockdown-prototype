48f72c6bfa12d551d0be10f7c576fe086bbb49ef
Simon Whatley <simonwhatley@gmail.com>
2020-10-10 21:43:09 +0100

    Version 3

--- app/models/postcode.js
+++ app/models/postcode.js
@@ -1,46 +1,49 @@
 'use strict'
 
 const mongoose = require('mongoose')
+const mongooseFuzzySearching = require('mongoose-fuzzy-searching')
 
 const Schema = mongoose.Schema
 
 const PostcodeSchema = new Schema({
   postcode: {type: String, required: true, unique: true, uppercase: true, trim: true},
   postcode_key: {type: String, required: true, unique: true, uppercase: true, trim: true},
-  in_use: Boolean,
+  postcode_area: String,
+  postcode_district: String,
   latitude: Number,
   longitude: Number,
+  altitude: Number,
   easting: Number,
   northing: Number,
   grid_ref: String,
-  county: String,
-  district: String,
-  ward: String,
-  district_code: String,
-  ward_code: String,
   country: String,
+  county: String,
   county_code: String,
+  district: String,
+  district_code: String,
+  ward: String,
+  ward_code: String,
   constituency: String,
-  introduced: Date,
-  terminated: Date,
+  constituency_code: String,
+  region: String,
   parish: String,
   built_up_area: String,
   built_up_sub_division: String,
   lower_layer_super_output_area: String,
-  region: String,
-  altitude: Number,
+  middle_layer_super_output_area: String,
   london_zone: Number,
   lsoa_code: String,
   local_authority: String,
   msoa_code: String,
-  middle_layer_super_output_area: String,
   parish_code: String,
   census_output_area: String,
-  constituency_code: String,
-  last_updated: Date,
-  postcode_area: String,
-  postcode_district: String,
-  plus_code: String
+  plus_code: String,
+  in_use: Boolean,
+  introduced: Date,
+  terminated: Date,
+  last_updated: Date
 })
+
+PostcodeSchema.plugin(mongooseFuzzySearching, { fields: ['constituency', 'county', 'district', 'postcode', 'region', 'ward'] });
 
 module.exports = mongoose.model('Postcode', PostcodeSchema)
